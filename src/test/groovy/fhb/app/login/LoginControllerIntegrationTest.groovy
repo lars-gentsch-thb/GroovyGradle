0592c4c623ea4c2b9e9c6a279999f46995edc72e
lgentsch <lars.gentsch@epost-dev.de>
2013-10-15 10:54:46 +0200

    Added integrationTest task and a simple test.

---
+++ src/test/groovy/fhb/app/login/LoginControllerIntegrationTest.groovy
@@ -0,0 +1,68 @@
+package fhb.app.login
+
+import fhb.app.login.LoginController;
+import fhb.app.login.LoginService;
+import fhb.app.login.UserNotAuthenticatedException;
+import spock.lang.Specification
+
+
+class LoginControllerIntegrationTest extends Specification {
+
+    static final String VALID_USERNAME = 'valid_username'
+    static final String VALID_PASSWORD = 'valid_password'
+    static final String INVALID_PASSWORD = 'invalid_password'
+    static final String INVALID_USERNAME = 'invalid_username'
+    static final boolean USER_AUTHENTICATED = true
+    static final boolean USER_NOT_AUTHENTICATED = false
+
+    static final NO_USERNAME = null
+    static final NO_PASSWORD = null
+
+    static final String EMPTY_USERNAME = ''
+    static final String EMPTY_PASSWORD = ''
+
+    LoginService serviceMock = Mock(LoginService)
+    LoginController controller = new LoginController(loginService: serviceMock)
+
+    void 'Login when user tries to login with valid credentials' () {
+        given: loginServiceAllowsAccessFor VALID_USERNAME, VALID_PASSWORD
+        expect: userDoesLoginWith VALID_USERNAME, VALID_PASSWORD
+    }
+
+    void 'Expect exception when user tries to login with invalid credentials'() {
+        given: loginServiceDeniesAccessFor INVALID_USERNAME, INVALID_PASSWORD
+        when: userDoesLoginWith INVALID_USERNAME, INVALID_PASSWORD
+        then: def exception = thrown(UserNotAuthenticatedException)
+    }
+
+    void 'Expect IllegalArgumentException when no credentials are given'() {
+        when: userDoesLoginWith NO_USERNAME, NO_PASSWORD
+        then: def exception = thrown(IllegalArgumentException)
+    }
+
+    void 'Expect IllegalArgumentException when empty credentials are given'() {
+        when: userDoesLoginWith EMPTY_USERNAME, EMPTY_PASSWORD
+        then: def exception = thrown(IllegalArgumentException)
+    }
+
+    // given
+    private void loginServiceDeniesAccessFor(username, password) {
+        serviceMock.login(username, password) >> USER_NOT_AUTHENTICATED
+    }
+
+    private void loginServiceAllowsAccessFor(username, password) {
+        serviceMock.login(username, password) >> USER_AUTHENTICATED
+    }
+
+    // when
+    private void userDoesLoginWith(username, password) {
+        controller.doLogin(username, password)
+    }
+
+}
+
+/**
+ * expect: wird verwendet für void Methodenaufruf
+ * thrown(): zum abfangen von erwarteten Exception
+ * loginServiceDeniesAccessFor und loginServiceAllowsAccessFor UseCase-spezifische given-Methoden
+ */
